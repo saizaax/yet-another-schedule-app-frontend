efbc4e0621a6d741f3750603a6192ba5aa5038cc
saizaax <flamesuspect@gmail.com>
2022-10-22 02:22:43 +0300

    Changed API host URL.

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-  baseURL: "https://schedule.saizaax.online/"
+  baseURL: "https://schedule.saizaax.xyz/"
 })
 
 export const fetcher = (url: string) => API.get(url).then((res) => res.data)
